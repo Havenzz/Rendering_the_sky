c04b4d6b4a1575730f29503ad605256b804ab4a1
logic <humorouslogic@outlook.com>
2023-03-04 13:52:23 +0800

    update files

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,6 +1,24 @@
 import axios from 'axios';
 
+axios.defaults.baseURL = 'http://localhost:3000'
+
 export const MockUrl = axios.create({
     baseURL:'https://www.fastmock.site/mock/79cad79f260f92472890423eddf0c375/caretblog',
     timeout:5000
 })
+
+export const getArtcile = (articleData:any) => {
+    axios.get('/article',articleData).then(res => res.data)
+}
+
+export const addArtcile = (articleData:any) => {
+    axios.post('/article',{ params: articleData }).then(res => res.data)
+}
+
+export const updateArtcile = (articleData:any) => {
+    axios.patch(`/article/${articleData.id}`,articleData).then(res => res.data)
+}
+
+export const deleteArtcile = (articleDataId:any) => {
+    axios.post(`/article/${articleDataId}`).then(res => res.data)
+}
